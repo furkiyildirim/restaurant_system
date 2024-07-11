cba04067901966b6939358f9d8276f7c83acd14a
Furkan YILDIRIM <141413430+furkiyildirim@users.noreply.github.com>
2024-07-11 22:30:46 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ const server_configuration = {
     port: 3000
 }
 // database configuration parameters
-const database_configuration = {
-    uri: 'mongodb://localhost:27017/restaurant_system'
-}
+//const database_configuration = {
+//    uri: 'mongodb://localhost:27017/restaurant_system'
+//}
 
-module.exports = {server_configuration, database_configuration}
+module.exports = {server_configuration}
